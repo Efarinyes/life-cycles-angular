b108582a558c8425637bce82e22c66ac58246536
Eduard Farinyes <efarinyes@gmail.com>
2023-08-22 18:20:49 +0200

    Primer Commit - Life Cycles Angular Exemples

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styles: [
+  ]
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+
+  public price: number = 0
+  public interval$?: Subscription
+
+  ngOnInit(): void {
+    console.log('Price Componenr: ngOnInit');
+
+   this.interval$ =  interval(1000).subscribe( value => console.log(`Tick: ${value}`))
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Price Componenr: ngOnChanges');
+    console.log({changes})
+  }
+  ngOnDestroy(): void {
+    console.log('Price Componenr: ngOnDestroy');
+    this.interval$?.unsubscribe()
+  }
+}
